286e6e2ada452b4e9317995b69267bf46d84db78
Sahil Udar <udarsahil0@gmail.com>
2025-08-01 12:46:28 +0530

    basic gui

--- main.py
+++ main.py
@@ -3,6 +3,29 @@ from psutil_functions import SystemMeasure
 
 current = SystemMeasure()
 
-print(current.returnAll())
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        super().resizable(False, False)
+        super().geometry("500x400")
+        super().title("CPU Util")
+        self.mainLabel = tk.Label()
+        self.mainLabel.pack()
+
+    def mainProg(self):
+        self.update()
+        
+    def update(self):
+        self.stats = current.returnAll()
+        self.mainLabel.config(text=str(self.stats['RAMUsagePercentage'])+" "+str(self.stats['AvailableRAM']))
+        self.after(800, self.update)
+
+
+
+
+app = App()
+app.mainProg()
+app.mainloop()
+
 
 
